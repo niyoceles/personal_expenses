3fbad2b40e49473b87958467f904278f2898ba19
Celestin Niyonsaba <niyoceles3@gmail.com>
2021-06-17 17:46:55 +0200

    add adaptive button for each platform

--- lib/main.dart
+++ lib/main.dart
@@ -162,7 +162,10 @@ class _MyHomePageState extends State<MyHomePage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('Show Chart'),
+                  Text(
+                    'Show Chart',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
                   Switch.adaptive(
                       activeColor: Theme.of(context).accentColor,
                       value: _showChart,
